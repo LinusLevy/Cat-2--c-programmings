a35b555d22fefb8e7a09463f7bce91dff21dedc5
LinusLevy <linuswerangai2005@gmail.com>
2024-12-02 07:06:55 +0300

    Update

---
+++ quiz 1.c
@@ -0,0 +1,53 @@
+Write a program that requests the user to enter hours worked in a week and the hourly wage. The program should then print the gross pay, the taxes, and the net pay. Assume the following:
+
+1.     Overtime (in excess of 40 hours for week) = time and a half.
+
+2.     Tax rate: 15% of the first $600, 20% of the rest.
+
+
+Answer text Question 1
+Subject: Payroll Calculation Program Development
+
+
+
+Dear [ Recepients Name],
+
+I hope this email finds you well. I am writing to inform you about thee completion of the payroll calculation program that was requested. The program has been successfully developed to facilitate the calculation of the gross pay, taxes,and net pay based on the hours worked in a week and the hourly wage input by the user.
+
+
+
+Here are the assumptions considered in the program:
+
+1.Overtime (in excess of forty hours for the week) is calculated at the rate of time and a half)
+
+2.Tax rate: 15% of the first $600, and 20% of the rest.
+
+
+
+The program functions as follows:
+
+1.The user is prompted to enter the hours worked in a week and the hourly wage.
+
+2.Based on the input provided, the program calculates the gross pay,taxes, and net pay.
+
+3.Overtime hours are considered for calculating the gross pay.
+
+4.Taxes are calculated according to the specified tax rates.
+
+5.Net pay is calculated as gross pay minus taxes.
+
+
+
+Thank you for your attention to this matter. I believe that this program will be beneficial in streamlining the payroll calculation process an ensuring accurate results for employees. If you require any further assistance or information regarding the program, please feel free to reach out to me.
+
+
+
+Best regards,
+
+
+
+Linus Levy
+
+The Manager
+
+0799574862.
